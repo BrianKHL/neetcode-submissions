8ba8d8f07df4637cdbab871d5c3c27aaf716b693
Brian Lee <129114119+BrianKHL@users.noreply.github.com>
2026-05-11 08:30:40 -0500

    Add: python-control-flow - submission-0

---
+++ Python For Beginners/python-control-flow/submission-0.py
@@ -0,0 +1,18 @@
+for i in range(1, 8):
+    pass #move on to the next 
+
+if True:
+    pass #move on to the next 
+
+def unfinsished_function():
+    pass #move on to the next 
+
+for i in range(1, 8):
+    break #exit out from the for loop 
+    print(i)
+
+for i in range(1, 8):
+    continue #move on to the next 
+    print(i)
+
+print("nothing else happened") # Print out 
